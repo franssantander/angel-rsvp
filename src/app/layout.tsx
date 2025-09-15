b17302e2a0a2c5a83f07929e2478f5d331a1131c
francesantander <santanderfrancisbeam@gmail.com>
2025-09-16 02:14:45 +0800

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ const dancingScript = Dancing_Script({
 export const metadata: Metadata = {
   title: "Angel's 18th Birthday",
   description: "RSVP for Angel’s Debut Celebration 🎉",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿</text></svg>",
+  },
 };
 
 export default function RootLayout({
